691a6cf7a988b53966cebe410e1c53fb7ae0f5d7
BuildTools <unconfigured@null.spigotmc.org>
2018-10-17 16:47:19 +0200

    Update Player Chat mention

--- src/fr/hugob147/endorialobby/listeners/PlayerChat.java
+++ src/fr/hugob147/endorialobby/listeners/PlayerChat.java
@@ -1,6 +1,7 @@
 package fr.hugob147.endorialobby.listeners;
 
 import fr.hugob147.endorialobby.rank.Rank;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,6 +13,16 @@ public class PlayerChat
 	@EventHandler public void onChat(AsyncPlayerChatEvent e)
 	{
 		Player p = e.getPlayer();
+		String msg = e.getMessage();
+
+		for(Player a : Bukkit.getOnlinePlayers())
+		{
+			if(msg.contains("@" + a.getCustomName()) || msg.contains("@" + a.getDisplayName()) || msg.contains("@" + a.getName()))
+			{
+				a.sendMessage("§eTu as été §cmentionné §epar : §a" + new Rank().getRank(p).getPrefix() + p.getDisplayName());
+			}
+		}
+
 		e.setFormat(new Rank().getRank(p).getPrefix() + "%1$s §7: §f%2$s");
 	}
 }
